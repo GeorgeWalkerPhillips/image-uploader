7ae6114508210effc4b8ade1c857c4510382e017
neeWat <103757294+neeWat@users.noreply.github.com>
2025-04-11 16:13:29 +0200

    Fixes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import FileUploadPage from "./Home"; // Your current upload UI
 import CameraCapture from "./CameraCapture";   // The camera UI
+import Home from "./Home";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<FileUploadPage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/camera" element={<CameraCapture />} />
       </Routes>
     </div>
